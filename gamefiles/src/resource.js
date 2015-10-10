16ddc6b1826dfbe45f72e73a18c3b04074c83c0a
Rajeeb Cherckal <vrajeeb@zynga.com>
2015-10-10 13:59:42 +0530

    added toggle logic for selecting honey

--- gamefiles/src/resource.js
+++ gamefiles/src/resource.js
@@ -1,9 +1,9 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
-
     CloseNormal_png : "res/StartScreen.png",
     CellNormal_png : "res/Cell_Empty.png",
     CellPressed_png : "res/Cell_Closed.png",
+    CellBee_png : "res/Cell_Bee.png",
     CellDisabled_png : "res/Cell_Honey.png",
     BG_png:"res/BG.png",
     Forest_BG_png:"res/ForestBG.png",
