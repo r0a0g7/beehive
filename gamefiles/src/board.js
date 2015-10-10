6624e64f98ca3a1393cd8778b6c3b4af3121ba29
nragam <nragam@zynga.com>
2015-10-10 15:32:28 +0530

    Pushing syntax errors

--- gamefiles/src/board.js
+++ gamefiles/src/board.js
@@ -76,11 +76,9 @@ var BoardLayer = cc.Layer.extend({
 
             this.addChild(cell.getCellSprite(), 3);
 
+            cell.bomb = 0;
             //cc.log("Adding PLayer");
             _maps[x, y] = cell;
-            cc.log(_maps[x, y]);
-            _maps[x,y].sethasMoved();
-            cc.log(_maps[x,y].gethasMoved());
 
         }
         catch (err) {
@@ -89,52 +87,30 @@ var BoardLayer = cc.Layer.extend({
         }
     },
     neighbourLogic : function() {
-        for (var x=0;x<this.CELLX;CELLX++){
-            for (var y=0;y<this.CELLY;CELLY++){
-                var c = this.map[x][y];
-                if (x>0){
-                    c.n1 = this.map[x-1][y];
-                    if (y>0)
-                        c.n2 = this.map[x-1][y-1];
-                    if (y<CELLY-1)
-                        c.n3 = this.map[x-1][y+1];
-                }
-                if (y>0)
-                    c.n4 = this.map[x][y-1];
-                if (y<this.CELLY-1)
-                    c.n5 = this.map[x][y+1];
-                if (x<this.CELLX-1)
-                    c.n6 = this.map[x+1][y];
-
-                //odd lines:
-                if (y%2){
-                    if (x<this.CELLX-1){
-                        if (y>0)
-                            c.n2 = this.map[x+1][y-1];
-                        if (y<this.CELLY-1)
-                            c.n3 = this.map[x+1][y+1];
-                    } else {
-                        c.n2 = false;
-                        c.n3 = false;
-                    }
-                }
+        cc.log("Inside neighbour function");
+        for (var x=0; x<CELLX;x++){
+            for (var y=0;y<CELLY;y++){
+                _maps[x, y].bomb = 2;
             }
         }
     },
 
     fixBees : function() {
-
+        cc.log("Inside Bee function");
         this.n_of_bombs=0;
         this.n_of_flags = 0;
         this.n_of_open_cells = 0;
-        for (var x=0;x<this.CELLX;x++){
-            for (var y=0;y< this.CELLY;y++){
-                this.map[x][y].init();
-                if (Math.random() < this.PERCENTAGE){
-                    this.map[x][y].bomb=1;
+        for (var x=0;x<CELLX;x++){
+            cc.log("Inside Bee for");
+            for (var y=0;y<CELLY;y++){
+                cc.log("Inside Bee function");
+                if (Math.random() < PERCENTAGE){
+                    _maps[x, y].bomb=1;
+                    cc.log("Bee value"+_maps[x, y].bomb );
                     this.n_of_bombs++;
                 } else {
-                    this.map[x][y].bomb=0;
+                    cc.log("Bee value"+_maps[x, y].bomb );
+                    _maps[x, y].bomb=0;
                 }
             }
         }
