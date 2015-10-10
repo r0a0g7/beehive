650ed4b970a02b9fd87d512e1a8c760c8753bb1b
sshreedhara <sshreedhara@zynga.com>
2015-10-10 11:10:05 +0530

    added the forest file

--- gamefiles/src/board.js
+++ gamefiles/src/board.js
@@ -6,40 +6,43 @@ var BoardLayer = cc.Layer.extend({
     ctor:function () {
         //////////////////////////////
         // 1. super init first
-        this._super();
-        this._cells = new Array;
+        try {
+            this._super();
+            this._cells = new Array;
 
-        for (i = 0; i < CELLX; i++) {
-            for(j=0;j< CELLY ;j++) {
-                this.addCell(i, j);
+            for (i = 0; i < CELLX; i++) {
+                for (j = 0; j < CELLY; j++) {
+                    this.addCell(i, j);
+                }
             }
+
+            //just check the first cell in array got the value for hasBall
+            var doescellhaveball = this._cells[0].getHasBall();
+            // cc.log(doescellhaveball);
+
+            /////////////////////////////
+            // 2. add a menu item with "X" image, which is clicked to quit the program
+            //    you may modify it.
+            // ask the window size
+            var size = cc.winSize;
+
+            var sprite = new cc.Sprite.create(res.HelloWorld_png);
+            // sprite.setAnchorPoint(cc.p(0.5, 0.5));
+            sprite.setPosition(cc.p(size.width / 2, size.height / 2));
+            this.addChild(sprite, 0);
+            /*var menuItemPlay = new cc.MenuItemSprite(
+             new cc.Sprite(res.ck2), // normal state image
+             new cc.Sprite(res.ck2), // select state image
+             this.onPlay, this);
+
+
+             var menuItem1 = new cc.MenuItemFont("Pop", pop);
+             var menu = new cc.Menu(menuItem1);
+             menu.alignItemsVertically();
+             this.addChild(menu, 1);*/
+        }catch(err){
+            cc.log(err);
         }
-
-        //just check the first cell in array got the value for hasBall
-        var doescellhaveball = this._cells[0].getHasBall();
-       // cc.log(doescellhaveball);
-
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
-        var size = cc.winSize;
-
-       var sprite = new cc.Sprite.create(res.HelloWorld_png);
-       // sprite.setAnchorPoint(cc.p(0.5, 0.5));
-        sprite.setPosition(cc.p(size.width / 2, size.height / 2));
-        this.addChild(sprite, 0);
-        /*var menuItemPlay = new cc.MenuItemSprite(
-            new cc.Sprite(res.ck2), // normal state image
-            new cc.Sprite(res.ck2), // select state image
-            this.onPlay, this);
-
-
-        var menuItem1 = new cc.MenuItemFont("Pop", pop);
-        var menu = new cc.Menu(menuItem1);
-        menu.alignItemsVertically();
-        this.addChild(menu, 1);*/
-
         return true;
     },
 
